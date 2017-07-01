fe955bc6a1a797681d42aeec5846ee90c297ae1b
camila-frangp <camila.frangp@gmail.com>
2017-07-01 13:36:32 -0400

    Creacion de select con listado de paises e input sin estilos

---
+++ assets/js/main.js
@@ -0,0 +1,5 @@
+//Funciones index2 --> signup
+$(".dropdown-menu li a").click(function(){
+  var selText = $(this).text();
+  $(this).parents('.btn-group').find('.dropdown-toggle').html(selText+' <span class="caret"></span>');
+});

e6b8eeaf26a0c87b465879c5e8dda7db626506b8
RangiBecerra <rangibecerra@gmail.com>
2017-07-02 14:27:10 -0400

    funcionalidad a pagina 2

---
+++ assets/js/main.js
@@ -0,0 +1,9 @@
+//index.html
+var elemento = document.getElementById("sign");
+elemento.addEventListener("click", signup);
+
+function signup() {
+    // validacion click boton Sign Up
+ 	window.location.href = "index2.html";
+};
+// Fin index.html

5998fbe1121feee45027bde56059f5e37303b3ff
Paulina Aros <aros.pau@gmail.com>
2017-07-03 13:06:23 -0400

    se agregan funciones para validar espacios vacios

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,4 @@
-var expr = /^[a-zA-Z0-9_\.\-]+@[a-zA-Z0-9\-]+\.[a-zA-Z0-9\-\.]+$/;
-
+/*
 $(document).ready(function(){
 
 	$("#enviar").click(function(){
@@ -7,17 +6,65 @@ $(document).ready(function(){
 		var correo = $("#email").val();
 
 		if(name == ""){
-			$("mensaje1").fadeIn();
+			$('#alert').html('debes  ingresar tu nombre').slideDown(500);
+			$('#nombre').focus();
 			return false;
 		}else{
-			$("mensaje1").fadeOut();
-			if(correo == "" || !expr.test(correo)){
-				$("mensaje2").fadeIn();
-				return false;
-			}else{
-				$("mensaje2").fadeOut();
+      $('alert').html('').slideUp(300);
+       }
 
-			}
-		}
+}
 	});
+
+
 });
+
+*/
+
+
+
+function validarNombre(){
+   var nombre = document.getElementById("nombre").value;
+   
+   if (nombre == ""){
+      $('#alert').html('debes  ingresar tu nombre').slideDown(500);
+      /*se pone slideDown para q aparezca como animacion en 500 milisegundos */
+      $('#nombre').focus(); /*con focus: se situa el cursor y no pasara al 
+      siguiente campo hasta q se cumpla la condicion*/
+      return false;
+   }else{
+      $('alert').html('').slideUp(300);
+      /* slideUp se escondera el mensaje*/
+   }
+
+}
+
+function validar(){
+   var email = document.getElementById("email").value;
+   
+   if (email == ""){
+      $('#alert').html('debes  ingresar tu correo electronico').slideDown(500);
+      /*se pone slideDown para q aparezca como animacion en 500 milisegundos */
+      $('#email').focus(); /*con focus: se situa el cursor y no pasara al 
+      siguiente campo hasta q se cumpla la condicion*/
+      return false;
+   }else{
+      $('alert').html('').slideUp(300);
+      /* slideUp se escondera el mensaje*/
+   }
+
+}
+
+$(document).ready(function() {
+    $("#enviar").on("click", function() {
+        var condiciones = $("#aceptar").is(":checked");
+        if (!condiciones) {
+            alert("Debe aceptar las condiciones");
+            event.preventDefault();
+        }
+    });
+    
+});
+
+
+
